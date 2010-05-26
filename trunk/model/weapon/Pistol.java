00277b4495b9a319460430bf65e03fa267e16072
jelle.voost@home.nl <jelle.voost@home.nl@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-26 11:57:08 +0000

    Added constants to Weapon and to Bullet.

--- trunk/model/weapon/Pistol.java
+++ trunk/model/weapon/Pistol.java
@@ -1,5 +1,13 @@
 package model.weapon;
 
+import model.bullet.Bullet;
+import model.bullet.PistolBullet;
+
 public class Pistol extends Weapon {
 
+	private final float FIRE_RATE = 1.50F;
+	private final Bullet BULLETTYPE = new PistolBullet();
+	private final boolean UNLIMITED_BULLETS = true;
+	
+	
 }
--- trunk/model/weapon/Weapon.java
+++ trunk/model/weapon/Weapon.java
@@ -1,5 +1,25 @@
 package model.weapon;
 
-public class Weapon {
+import model.bullet.*;
 
+public abstract class Weapon {
+
+	protected final float FIRE_RATE = 1; // The amount of bullets per second
+	protected final Bullet BULLETTYPE = null; 
+	protected final boolean UNLIMITED_BULLETS = false;
+	protected final int MAX_BULLETS = 0;
+	protected int currentBullets = 0;
+	
+	// TODO Implement Shoot
+	public Bullet shoot(){
+		if(UNLIMITED_BULLETS || currentBullets > 0){
+			//Shoot a bullet
+			if(!UNLIMITED_BULLETS){
+				currentBullets--;
+			}
+			return BULLETTYPE;
+		}
+		return null;
+	}
+	
 }
