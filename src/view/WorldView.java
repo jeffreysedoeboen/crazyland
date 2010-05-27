bd417e05d06078984d80d52cc1bf3917fb2b65fb
marco.beierer <marco.beierer@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 12:48:43 +0000

    world view

--- src/view/WorldView.java
+++ src/view/WorldView.java
@@ -9,20 +9,22 @@ import model.World;
 
 public class WorldView implements Observer {
 
-	private GameServer server;
+	//private GameServer server;
+	private World world;
+	private Map map;
 	
-	public WorldView(World world){
+	public WorldView(World world) {
+		this.world = world;
 		world.addObserver(this);
+	}
+	
+	public void getMap() {
+		map = world.getMap();
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
+
 		//tikmer wat is de map, map tekenen, waar staan alle spelers, teken alle spelers.
 	}
 }
