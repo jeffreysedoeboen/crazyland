85a844e054c168a3d33cdb95c683aa5e11cdbf69
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 09:20:35 +0000

    Player attributen toegevoegd en shoot methode gemaakt

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,7 +1,30 @@
 package model;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
+import model.weapon.Pistol;
+import model.weapon.Weapon;
+
 public class Player {
 
 	protected String name;
+	protected Point position;
+	protected ArrayList<Weapon> weapons;
 	
-}  
+	public Player( String name, Point position ) {
+		this.weapons  = new ArrayList<Weapon>();
+		this.name     = name;
+		this.position = position;
+		
+		//standaart pistool
+		weapons.add(new Pistol());
+	}
+	
+	public void shoot() {
+		if(weapons.size() > 0) {
+			weapons.get(0).shoot();
+		}
+	}
+	
+}
