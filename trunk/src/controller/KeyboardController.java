5a6e355e489cf2e80856e3d4bc04f593dfabbe13
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 11:02:50 +0000

    Controllers geupdate

--- trunk/src/controller/KeyboardController.java
+++ trunk/src/controller/KeyboardController.java
@@ -1,5 +1,31 @@
 package controller;
 
-public class KeyboardController {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardController implements KeyListener {
+
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case(KeyEvent.VK_SPACE): //TODO springen
+			break;
+		case(KeyEvent.VK_A): //TODO naar links lopen
+			break;
+		case(KeyEvent.VK_D): //TODO naar rechts lopen
+			break;
+		case(KeyEvent.VK_TAB): //TODO score laten zien
+			break;
+		case(KeyEvent.VK_ESCAPE): System.exit(0);
+			break;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
 
 }
--- trunk/src/controller/MouseController.java
+++ trunk/src/controller/MouseController.java
@@ -1,5 +1,58 @@
 package controller;
 
-public class MouseController {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import model.Player;
+
+public class MouseController implements MouseListener, MouseMotionListener{
+	private Player player;
+	
+	public MouseController(Player player) {
+		this.player = player;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO schieten
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+		
+	}
 
 }
