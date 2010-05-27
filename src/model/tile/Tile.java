46517c77bf79e678ffc973ba53fb03c368092088
jelle.voost@home.nl <jelle.voost@home.nl@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 09:19:28 +0000

    edited tile class

--- src/model/tile/Tile.java
+++ src/model/tile/Tile.java
@@ -6,10 +6,24 @@ public class Tile {
 	private int x;
 	private int y;
 	private BufferedImage image;
+	private boolean solid;
 	
-	public Tile(int x, int y, BufferedImage image){
+	public Tile(int x, int y, BufferedImage image, boolean solid){
 		this.x = x;
 		this.y = y;
-		
+		this.solid = solid;
 	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public boolean isSolid(){
+		return this.solid;
+	}
+	
 }
