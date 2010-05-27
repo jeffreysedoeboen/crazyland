586eda7d5847847de77f6b2b28c29d8db053e909
marijnie28 <marijnie28@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 12:58:53 +0000

    map kan ingeladen worden

--- trunk/src/model/Mapfactory.java
+++ trunk/src/model/Mapfactory.java
@@ -1,9 +1,21 @@
 package model;
 
-import java.io.File;
+import it.sauronsoftware.base64.Base64;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.zip.GZIPInputStream;
+
+import javax.imageio.ImageIO;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import model.tile.Tile;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,32 +25,59 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 public class Mapfactory {
-	public static void getMap(String mapsource) {
+	private static BufferedImage originalimage;
+	
+	public static Tile[][] getMap(String mapsource) {
+		Tile[][] tiles = new Tile[10][10];
+		
 		try {
-
+			originalimage = ImageIO.read(new File("tiles/grass_main.png"));
+			ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+			/*for(int x = 0; x < 400; x += 64) {
+				for(int y = 0; y < 330; y += 64) {
+					images.add(originalimage.getSubimage(x, y, 64, 64));
+				}
+			}*/
+			images.add(originalimage.getSubimage(0, 0, 32, 32));
+			images.add(originalimage.getSubimage(0, 32, 32, 32));
+			images.add(originalimage.getSubimage(0, 64, 32, 32));
+			images.add(originalimage.getSubimage(0, 96, 32, 32));
+			images.add(originalimage.getSubimage(0, 128, 32, 32));
+			images.add(originalimage.getSubimage(0, 160, 32, 32));
+			images.add(originalimage.getSubimage(32, 0, 32, 32));
+			images.add(originalimage.getSubimage(32, 0, 32, 32));
+			images.add(originalimage.getSubimage(32, 0, 32, 32));
+			images.add(originalimage.getSubimage(32, 0, 32, 32));
+			images.add(originalimage.getSubimage(32, 0, 32, 32));
+			images.add(originalimage.getSubimage(32, 0, 32, 32));
+			images.add(originalimage.getSubimage(96, 32, 32, 32));
+			images.add(originalimage.getSubimage(96, 64, 32, 32));
+			images.add(originalimage.getSubimage(96, 96, 32, 32));
+			
+			
             DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
             Document doc = docBuilder.parse (new File("maps/" + mapsource + ".tmx"));
             doc.getDocumentElement ().normalize ();
-            NodeList listOfMaps = doc.getElementsByTagName("map");
-            int totalMaps = listOfMaps.getLength();
+            NodeList map = doc.getElementsByTagName("map");
 
-            for(int s=0; s<listOfMaps.getLength() ; s++){
-
-
-                Node firstMapNode = listOfMaps.item(s);
-                if(firstMapNode.getNodeType() == Node.ELEMENT_NODE){
-                	Element firstMapElement = (Element)firstMapNode;
-                	
-                	boolean solid = Boolean.valueOf(firstMapElement.getAttribute("solid")).booleanValue();
-                	
-                    NodeList firstNameList = firstMapElement.getElementsByTagName("data");
-                    Element firstNameElement = (Element)firstNameList.item(0);
-
-                    NodeList textFNList = firstNameElement.getChildNodes();
-                    String encodedBytes = ((Node)textFNList.item(0)).getNodeValue().trim();
-                }
+            NodeList listOfTiles = doc.getElementsByTagName("tile");
+            
+            int x = 0;
+            int y = 0;
+            for(int s = 0; s < listOfTiles.getLength(); s++) {
+            	Node firstPersonNode = listOfTiles.item(s);
+            	Element firstPersonElement = (Element)firstPersonNode;
+            	
+            	int tiletype = Integer.valueOf(firstPersonElement.getAttribute("gid")).intValue();
+            	tiles[x][y] = new Tile(x,y,images.get(tiletype), true);
+            	x += 1;
+            	if(x % 10 == 0) {
+            		x = 0;
+            		y += 1;
+            	}
             }
+
 		} catch (SAXParseException err) {
         System.out.println ("** Parsing error" + ", line " 
              + err.getLineNumber () + ", uri " + err.getSystemId ());
@@ -51,5 +90,7 @@ public class Mapfactory {
         }catch (Throwable t) {
         t.printStackTrace ();
         }
+        
+        return tiles;
 	}
 }
