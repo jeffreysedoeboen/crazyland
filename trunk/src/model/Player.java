c152728c9dc78a71d15bfa44ee941a9adc0782de
denny_leussink@hotmail.com <denny_leussink@hotmail.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 11:10:30 +0000

    player methods added

--- trunk/src/model/Player.java
+++ trunk/src/model/Player.java
@@ -1,7 +1,11 @@
 package model;
 
+import java.awt.Image;
 import java.awt.Point;
+import java.io.File;
 import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
 
 import model.weapon.Pistol;
 import model.weapon.Weapon;
@@ -11,11 +15,15 @@ public class Player {
 	private String name;
 	private Point position;
 	private Weapon weapon;
-	private int hitpoints;
+	private int hitpoints = 10;
+	private static Image playerImage;
 	
 	public Player( String name, Point position ) {
 		this.name     = name;
 		this.position = position;
+
+		ImageIcon playerImage2 = new ImageIcon("../themes/tee/character.png");
+		playerImage = playerImage2.getImage();
 		
 		//standaart pistool
 		setWeapon(new Pistol());
