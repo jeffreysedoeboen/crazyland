b8eba674a3d5fc8927b5ecec9c49bed5e23c4f51
jelle.voost@home.nl <jelle.voost@home.nl@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 07:58:06 +0000

    add read for the original image

--- model/Map.java
+++ model/Map.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Map {
 
+	private BufferedImage org;
+	
+	public Map(){
+		try {
+			org = ImageIO.read(new File("kaas.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
