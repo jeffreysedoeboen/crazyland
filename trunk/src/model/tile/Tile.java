9a22061cb8b71d7bb5d45e6e9d924da8294e3b5f
marijnie28 <marijnie28@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-27 08:51:02 +0000

    map en tile aangepast

--- trunk/src/model/tile/Tile.java
+++ trunk/src/model/tile/Tile.java
@@ -3,5 +3,13 @@ package model.tile;
 import java.awt.image.BufferedImage;
 
 public class Tile {
+	private int x;
+	private int y;
 	private BufferedImage image;
+	
+	public Tile(int x, int y, BufferedImage image){
+		this.x = x;
+		this.y = y;
+		
+	}
 }
