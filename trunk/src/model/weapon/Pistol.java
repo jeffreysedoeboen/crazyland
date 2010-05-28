5e2bbbfafe76370eab10c9932e88ec1b7b1d975d
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-28 10:16:19 +0000

    methodes voor schieten gemaakt

--- trunk/src/model/weapon/Pistol.java
+++ trunk/src/model/weapon/Pistol.java
@@ -10,13 +10,12 @@ import model.bullet.PistolBullet;
 public class Pistol extends Weapon {
 
 	private final float FIRE_RATE = 1.50F;
-	private final Bullet BULLETTYPE = new PistolBullet();
 	private final boolean UNLIMITED_BULLETS = true;
 	
 	
 	public Pistol() {
 		try {
-			this.image = ImageIO.read(new File("")); //TODO file nog aangeven
+			this.image = ImageIO.read(new File("../themes/tee/weapon/pistol.jpg")); //TODO file nog aangeven
 		} catch (IOException e) {
 			System.err.println("Error while reading image for weapon");
 			e.printStackTrace();
