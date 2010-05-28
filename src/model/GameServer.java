5d2c579bb7c31f9dbd46354026de39996c5c4f81
jelle.voost@home.nl <jelle.voost@home.nl@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-28 08:31:31 +0000

    player and stuff fixed

--- src/model/GameServer.java
+++ src/model/GameServer.java
@@ -1,11 +1,36 @@
 package model;
 
+import java.awt.Image;
 import java.awt.Point;
 
 public class GameServer {
 	
-		public boolean checkDirection(Point position, char direction){
-			return true;
-		}
+	World world;
+	
+	public GameServer(){
+		
+		world = new World();
+		
+	}
+	
+	public World getWorld(){
+		return this.world;
+	}
+
+	public Point getPlayerPosition(){
+		return this.world.getPlayerPosition();
+	}
+	
+	public void jump() {
+		// TODO Creating jump
+		world.playerJump();
+		
+	}
+
+	public Image getPlayerImage() {
+
+		return world.getPlayerImage();
+	
+	}
 		
 }
--- src/view/WorldView.java
+++ src/view/WorldView.java
@@ -1,31 +1,45 @@
 package view;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.io.File;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+import model.GameServer;
 import model.Map;
 import model.World;
 import model.tile.Tile;
 
 public class WorldView extends JPanel {
 
-	//private GameServer server;
-	private World world; 
-	private Map map;
+	private GameServer server;
 	
-	public WorldView(World world) {
-		this.world = world;
-		map = world.getMap();
+	public WorldView(GameServer server) {
+		this.server = server;
 	}
 	
 	public void paintComponent(Graphics g){
 		//TODO tiles tekenen kan nog verbeterd worden
 		
 		super.paintComponent(g);
-		for(Tile[] rows : map.getTiles()) {
+		for(Tile[] rows : server.getWorld().getMap().getTiles()) {
 			for(Tile tile : rows) {
 				g.drawImage(tile.getImage(), tile.getX()*32, tile.getY()*32, null);
 			}
 		}
+		
+		Point s = server.getPlayerPosition();
+
+		g.drawImage(server.getPlayerImage(),((int)s.getX())*32,((int)s.getY())*32,null);
+		
+		System.out.println("VERF!");
 	}
+	
 }
