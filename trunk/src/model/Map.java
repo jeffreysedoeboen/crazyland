208f6ec926588a4de1270174d932792ea7c92d22
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-28 08:05:26 +0000

    mijn conflicten opgelost

--- trunk/src/model/Map.java
+++ trunk/src/model/Map.java
@@ -1,24 +1,6 @@
 package model;
 
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-
 import model.tile.Tile;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
 public class Map {
 	private Tile[][] tiles;
