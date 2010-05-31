64b9776c4142e5360f79d7336490ded8ae0736de
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-31 10:33:46 +0000

    contructor weer goed gemaakt van bullet en pistolbullet

--- trunk/src/model/bullet/PistolBullet.java
+++ trunk/src/model/bullet/PistolBullet.java
@@ -1,6 +1,5 @@
 package model.bullet;
 
-import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
 
@@ -9,10 +8,7 @@ import javax.imageio.ImageIO;
 public class PistolBullet extends Bullet {
 
 	
-	public PistolBullet(double direction, Point startPosition) {
-		super(direction, startPosition);
-		System.out.println("new pistol bullet");
-		
+	public PistolBullet() {		
 		try {
 			bulletImage = ImageIO.read(new File("../themes/tee/characters/character.png"));
 		} catch (IOException e) {
