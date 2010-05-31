00e164c86493f83805dd5da0a3e969ad6d2cf831
marijnie28 <marijnie28@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-31 09:59:45 +0000

    screen movement. Player in the middle

--- src/view/WorldView.java
+++ src/view/WorldView.java
@@ -28,23 +28,39 @@ public class WorldView extends JPanel {
 //			}
 //		}
 		
-		Tile[][] rows = server.getWorld().getMap().getTiles();
+		Tile[][] tiles = server.getWorld().getMap().getTiles();
 		
-		for(int y = 0; y < 10; y++){
-			for(int x = 0; x < 50; x++){
-				g.drawImage(rows[x][y].getImage(),x*32, y*32, null);
-			}
+		//nieuwe shit
+		int offsetX = this.getWidth() / 2 - Math.round(server.getPlayerX());
+		int mapWidth = tilesToPixels(server.getWorld().getMap().getWidth());
+		offsetX = Math.min(offsetX, 0);
+		offsetX = Math.max(offsetX, this.getWidth() - mapWidth);
+		
+		int offsetY = this.getHeight() - tilesToPixels(server.getWorld().getMap().getHeight());
+		int mapHeight = tilesToPixels(server.getWorld().getMap().getHeight());
+		offsetY = Math.min(offsetY, 0);
+		offsetY = Math.max(offsetY, this.getHeight() - mapHeight);
+		
+		int firstTileX = pixelsToTiles(-offsetX);
+		int lastTileX = firstTileX + pixelsToTiles(this.getWidth()) + 1;
+		int firstTileY = pixelsToTiles(-offsetY);
+		int lastTileY = firstTileY + pixelsToTiles(this.getHeight()) + 1;
+		
+		for (int y=firstTileY; y < lastTileY; y++) {
+		    for (int x=firstTileX; x < lastTileX; x++) {
+		        g.drawImage(tiles[x][y].getImage(),tilesToPixels(x) + offsetX, tilesToPixels(y) + offsetY, null);
+		    }
 		}
-		
+
 		float playerX = server.getPlayerX();
 		float playerY = server.getPlayerY();
 
-		g.drawImage(server.getPlayerImage(),(int)playerX,(int)playerY,null);
+		g.drawImage(server.getPlayerImage(),Math.round(playerX) + offsetX,Math.round(playerY) + offsetY,null);
 		
 		//TODO meerdere bullets
 		Bullet bullet = server.getBullet();
 		if(bullet != null) {
-			g.drawImage(server.getBulletImage(),(int) bullet.getPosition().getX(),(int) bullet.getPosition().getY(), null);
+			g.drawImage(server.getBulletImage(),(int) bullet.getPosition().getX() + offsetX,(int) bullet.getPosition().getY() + offsetY, null);
 		}
 		
 		//TODO meerder spelers
@@ -53,9 +69,20 @@ public class WorldView extends JPanel {
 			int midPlayerX = (int) (player1.getX() + ( player1.getImage().getWidth(null) / 2));
 			int midPlayerY = (int) (player1.getY() + ( player1.getImage().getHeight(null) / 2));
 			
-			g.drawImage(player1.getWeapon().getImage(), midPlayerX, midPlayerY, null);
+			g.drawImage(player1.getWeapon().getImage(), midPlayerX + offsetX, midPlayerY + offsetY, null);
 		}
 		
 	}
 	
+	public int pixelsToTiles(float pixels) {
+		return pixelsToTiles(Math.round(pixels));
+	}
+	
+	public int pixelsToTiles(int pixels) {
+		return (int) Math.floor((float) pixels / 32);
+	}
+	
+	public int tilesToPixels(int numTiles) {
+		return numTiles * 32;
+	}
 }
