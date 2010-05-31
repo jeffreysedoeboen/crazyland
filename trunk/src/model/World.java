6d63afd3a24e6120000a1967f841ac973167118f
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-05-31 09:34:10 +0000

    direction werkt!!!!!

--- trunk/src/model/World.java
+++ trunk/src/model/World.java
@@ -10,16 +10,16 @@ public class World{
 	Map map;
 	Player player;
 	Bullet bullet;
-	
+
 	public World(){
 		map = new Map();
 		player = new Player("Henk", 200f,100f);
 	}
-	
+
 	public Map getMap() {
 		return map;
 	}
-	
+
 	public float getPlayerX(){
 		return player.getX();
 	}
@@ -27,115 +27,113 @@ public class World{
 	public float getPlayerY(){
 		return player.getY();
 	}
-	
+
 	public Image getPlayerImage() {
-		
+
 		return player.getImage();
-		
+
 	}
-	
+
 	public void shoot(Point mouseDot) {
-		float playerX = getPlayerX();
-		float playerY = getPlayerY();
-		Point playerPosition = new Point((int)playerX, (int)playerY);
-		float direction;
-		float schuin =   (float) playerPosition.distance(mouseDot) ;
-		float overkant =  mouseDot.y - playerY;
-		float newDirection;
-		//float dx =  Math.abs(mouseDot.x - playerX);
-		//float dy =  Math.abs(playerY -  mouseDot.y);
-		direction = (float) Math.sin(overkant/schuin);
-		
-		if (direction < 0) {
-			direction = Math.abs(direction) + 3.60f;
-		}
-		
-//		if(dx > 0 && dy < 0) {
-//			newDirection = 180 - direction;
-//		} else if(dx < 0 && dy < 0) {
-//			newDirection = 270 - direction;
-//		} else if(dx < 0 && dy > 0) {
-//			newDirection = 360 - direction;
-//		}
-//		else {
-//			newDirection = direction;
-//		}
-		
-		
-		
-		//System.out.println("dx: " + dx);
-		//System.out.println("dy: " + dy);
-		System.out.println("Direction: " + direction *100);
-		
-		
-		
-		//TODO moet nog een lijst worden
-		bullet = player.shoot();
-	}
+		float playerX = getPlayerX() + 16;
+		float playerY = getPlayerY() + 16;
+		float direction = 0;
+		float dx =  Math.abs(playerX - mouseDot.x );
+		float dy =  Math.abs(playerY -  mouseDot.y);
 
-	public Image getBulletImage() {
-		return bullet.getBulletImage();
-	}
-	
-	public Player getPlayer() {
-		return player;
-	}
-
-	public void moveWeapon(Point mousePoint) {
-		player.getWeapon().turnToPoint(mousePoint);
-	}
-
-	public void move() {
-		if(player.isMovingLeft()){
-			player.moveLeft(onGround());
-		}else if(player.isMovingRight()){
-			player.moveRight(onGround());
+		dx = (float) (playerX - mouseDot.getX());
+		dy = (float) (playerX - mouseDot.getY());
+		double angle = 0.0d;
+		if (dx == 0.0) {
+			if(dy == 0.0)angle = 0.0;
+			else if(dy > 0.0) angle = Math.PI / 2.0;
+			else angle = (Math.PI * 3.0) / 2.0;
 		}
-		player.moveVertical();
-		player.calcVerticalSpeed(onGround());
-		if(collisionCeiling()){
-			player.setVerticalSpeed(-0.1f);
+		else if(dy == 0.0) {
+			if(dx > 0.0)angle = 0.0;
+			else angle = Math.PI;
 		}
-		System.out.println("Onground: " + onGround());
-		System.out.println("OnCeiling: " + collisionCeiling());
-		
-	}
-	
-	private boolean collisionCeiling() {
-		Tile[][] tiles = map.getTiles();
-		if(tiles[(int) (player.getX()/32)][(int) ((player.getY())/32)].isSolid() || tiles[(int) ((player.getX()+32)/32)][(int) ((player.getY())/32)].isSolid()){
-			return true;
+		else {
+			if(dx < 0.0)angle = Math.atan(dy/dx) + Math.PI;
+			else if(dy < 0.0) angle = Math.atan(dy/dx) + (2*Math.PI);
+			else angle = Math.atan(dy/dx);
 		}
-		return false;
-	}
+		direction = (float) ((angle * 180) / Math.PI);
 
-	public boolean onGround(){
-		Tile[][] tiles = map.getTiles();
-		if(tiles[(int) (player.getX()/32)][(int) ((player.getY()+32)/32)].isSolid() || tiles[(int) ((player.getX()+32)/32)][(int) ((player.getY()+32)/32)].isSolid()){
-			return true;
-		}
-		return false;
-	}
 
-	public void startMovingRight() {
-		player.setMovingRight(true);
-	}
-	
-	public void stopMovingRight() {
-		player.setMovingRight(false);		
-	}
-	
-	public void startMovingLeft() {
-		player.setMovingLeft(true);		
-	}
-	
-	public void stopMovingLeft() {
-		player.setMovingLeft(false);		
-	}
+//	System.out.println("dx: " + dx);
+//	System.out.println("dy: " + dy);
+//	System.out.println("Direction: " + direction);
 
-	public void jump() {
-		if(onGround()){
-			player.setVerticalSpeed(4.5f);
-		}
+
+
+	//TODO moet nog een lijst worden
+	bullet = player.shoot();
+}
+
+public Image getBulletImage() {
+	return bullet.getBulletImage();
+}
+
+public Player getPlayer() {
+	return player;
+}
+
+public void moveWeapon(Point mousePoint) {
+	player.getWeapon().turnToPoint(mousePoint);
+}
+
+public void move() {
+	if(player.isMovingLeft()){
+		player.moveLeft(onGround());
+	}else if(player.isMovingRight()){
+		player.moveRight(onGround());
 	}
+	player.moveVertical();
+	player.calcVerticalSpeed(onGround());
+	if(collisionCeiling()){
+		player.setVerticalSpeed(-0.1f);
+	}
+	//		System.out.println("Onground: " + onGround());
+	//		System.out.println("OnCeiling: " + collisionCeiling());
+
+}
+
+private boolean collisionCeiling() {
+	Tile[][] tiles = map.getTiles();
+	if(tiles[(int) (player.getX()/32)][(int) ((player.getY())/32)].isSolid() || tiles[(int) ((player.getX()+32)/32)][(int) ((player.getY())/32)].isSolid()){
+		return true;
+	}
+	return false;
+}
+
+public boolean onGround(){
+	Tile[][] tiles = map.getTiles();
+	if(tiles[(int) (player.getX()/32)][(int) ((player.getY()+32)/32)].isSolid() || tiles[(int) ((player.getX()+32)/32)][(int) ((player.getY()+32)/32)].isSolid()){
+		return true;
+	}
+	return false;
+}
+
+public void startMovingRight() {
+	player.setMovingRight(true);
+}
+
+public void stopMovingRight() {
+	player.setMovingRight(false);		
+}
+
+public void startMovingLeft() {
+	player.setMovingLeft(true);		
+}
+
+public void stopMovingLeft() {
+	player.setMovingLeft(false);		
+}
+
+public void jump() {
+	if(onGround()){
+		player.setVerticalSpeed(4.5f);
+	}
+}
 }
