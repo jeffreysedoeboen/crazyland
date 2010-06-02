b55197286f309e4b726126c084cef2283a487409
marco.beierer <marco.beierer@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-06-02 13:45:10 +0000

    server

---
+++ trunk/src/server/model/Sender.java
@@ -0,0 +1,47 @@
+package server.model;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import server.model.bullet.Bullet;
+
+public class Sender {
+
+	private PrintWriter out;
+	private Player player;
+	private String lineOut = "";
+		
+	public Sender(PrintWriter out, Player p){
+		this.out = out;
+		this.player = p;
+		out.println("begin_map\n" + "crazyland1");
+	}
+	
+	public void sendPlayer(){
+		lineOut += "player%n" + (int)player.getX() + "," + (int)player.getY() + "%n";
+	}
+	
+	public void sendPlayers(ArrayList<Player> playerList){
+		String kaas = "players_begin%n";
+		for(Player p : playerList){
+			if(p != this.player){
+				kaas += p.getName()+ "," + (int)p.getX() + "," + (int)p.getY() + "%n";
+			}
+		}
+		lineOut += kaas + "players_end%n";
+	}
+	
+	public void sendBullets(ArrayList<Bullet> bulletList){
+		String kaas = "bullets_begin%n";
+		for(Bullet b : bulletList){
+			kaas += (int)b.getX() + "," + (int)b.getY() + "%n";
+		}
+		lineOut += kaas + "bullets_end%n";
+	}
+	
+	public void sendLineOut(){
+		out.printf(lineOut);
+		lineOut = "";
+	}
+	
+}
---
+++ trunk/src/server/model/weapon/Pistol.java
@@ -0,0 +1,38 @@
+package server.model.weapon;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import server.model.bullet.Bullet;
+import server.model.bullet.PistolBullet;
+
+
+public class Pistol extends Weapon {
+
+	protected final Bullet bulletType = new PistolBullet();
+	protected float fireRate = 3F;
+	protected boolean unlimitedBullets = true;
+	
+	public Pistol() {	
+		try {
+			this.image = ImageIO.read(new File("themes/tee/weapon/gun.png"));
+		} catch (IOException e) {
+			System.err.println("Error while reading image for weapon");
+			e.printStackTrace();
+		}
+	}
+
+	public boolean isUnlimitedBullets() {
+		return unlimitedBullets;
+	}
+	
+	public Bullet createBullet(){
+		return new PistolBullet();
+	}
+	
+	public float getFireRate(){
+		return this.fireRate;
+	}
+	
+}
