10d5a3108adfa3da20851d108f4bbca9846f3fff
bappie6@msn.com <bappie6@msn.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-06-07 13:21:11 +0000

    Leaderboard dao

---
+++ src/db/LBPlayer.java
@@ -0,0 +1,54 @@
+package db;
+
+public class LBPlayer {
+	private long rank, kills, deaths;
+	private String userName;
+	
+	public LBPlayer(long rank, String userName, Long kills, long deaths) {
+		this.rank 		= rank;
+		this.userName 	= userName;
+		this.kills 		= kills;
+		this.deaths 	= deaths;
+	}
+
+	public long getRank() {
+		return rank;
+	}
+
+	public void setRank(long rank) {
+		this.rank = rank;
+	}
+
+	public long getKills() {
+		return kills;
+	}
+
+	public void setKills(long kills) {
+		this.kills = kills;
+	}
+
+	public long getDeaths() {
+		return deaths;
+	}
+
+	public void setDeaths(long deaths) {
+		this.deaths = deaths;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String toString() {
+		return "" + rank + " " + userName + " " + kills + " " + deaths;
+	}
+
+	public float getKd() {
+		return kills/deaths;
+	}
+
+}
