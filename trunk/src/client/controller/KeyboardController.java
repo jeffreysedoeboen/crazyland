65a6a8668d173bce95e3361bd32399b7bb77d344
denny_leussink@hotmail.com <denny_leussink@hotmail.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-06-09 09:05:55 +0000

    tussenstand

--- trunk/src/client/controller/KeyboardController.java
+++ trunk/src/client/controller/KeyboardController.java
@@ -4,51 +4,59 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import client.connection.Sender;
+import client.view.ScoreView;
+import client.view.WorldView;
 
 public class KeyboardController implements KeyListener {
 
 	private Sender sender;
-	
-	public KeyboardController(Sender sender) {
+	private ScoreView scoreview;
+
+	public KeyboardController(Sender sender, ScoreView scoreview) {
 		this.sender = sender;
+		this.scoreview = scoreview;
+
 	}
 
-	
 	public void keyPressed(KeyEvent e) {
-
+		switch(e.getKeyCode()) {
+		case(KeyEvent.VK_ENTER): //TODO score laten zien
+			scoreview.setBounds(100,100,100,100);
+			scoreview.setVisible(true);	
+			break;
+		}
 	}
 
 	public void keyReleased(KeyEvent e) {
 		switch(e.getKeyCode()) {
-			case(KeyEvent.VK_A):
-				sender.stopMovingLeft();
-				break;
-			case(KeyEvent.VK_D):
-				sender.stopMovingRight();
-				break;
-			case(KeyEvent.VK_TAB): //TODO score laten zien
-				break;
+		case(KeyEvent.VK_A):
+			sender.stopMovingLeft();
+		break;
+		case(KeyEvent.VK_D):
+			sender.stopMovingRight();
+		break;
+		case(KeyEvent.VK_ENTER): //TODO score laten zien
+			scoreview.setVisible(false);
+		break;
 		}
 	}
 
 	public void keyTyped(KeyEvent e) {
-		
+		System.out.println(e.getKeyChar());
 		switch(e.getKeyChar()) {
-			case(KeyEvent.VK_SPACE):
-				sender.jump();
-				break;
-			case('a'):
-				sender.startMovingLeft();
-				break;
-			case('d'):
-				sender.startMovingRight();
-				break;
-			case(KeyEvent.VK_TAB): //TODO score laten zien
-				break;
-			case(KeyEvent.VK_ESCAPE): 
-				sender.removePlayer();
-				System.exit(0);
-				break;
+		case(KeyEvent.VK_SPACE):
+			sender.jump();
+		break;
+		case('a'):
+			sender.startMovingLeft();
+		break;
+		case('d'):
+			sender.startMovingRight();
+		break;
+		case(KeyEvent.VK_ESCAPE): 
+			sender.removePlayer();
+		System.exit(0);
+		break;
 		}
 	}
 
---
+++ trunk/src/client/view/ScoreView.java
@@ -0,0 +1,16 @@
+package client.view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class ScoreView extends JPanel{
+	
+	public ScoreView() {
+		this.setBackground(Color.PINK);
+		this.setSize(100,100);
+		this.setVisible(false);
+	}
+}
