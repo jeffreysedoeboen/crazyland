4c59e674022b413b0dcb7e5ebccb7988b0f4e842
jelle.voost@home.nl <jelle.voost@home.nl@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-06-11 13:40:34 +0000

    asd

--- src/server/model/World.java
+++ src/server/model/World.java
@@ -221,15 +221,15 @@ public boolean checkCloseBulletColission(Bullet b, Shape tile){
 			Circle e1 = (Circle) tile;
 			Circle c1,c2,c3;
 			
-			c1 = new Circle(2,(int)shape.getCenterX()-2,(int)shape.getCenterY()-2);
+			c1 = new Circle(3,(int)shape.getCenterX()-3,(int)shape.getCenterY()-3);
 			
 			int posX = (int) (3*Math.cos(dir));
 			int posY = (int) (3*Math.sin(dir));
-			c2 = new Circle(2,(int)shape.getCenterX()+posX-2, (int)shape.getCenterY()+posY-2);
+			c2 = new Circle(3,(int)shape.getCenterX()+posX-3, (int)shape.getCenterY()+posY-3);
 			
 			posX = (int) (3*Math.cos(dir+Math.PI));
 			posY = (int) (3*Math.sin(dir+Math.PI));
-			c3 = new Circle(2,(int)shape.getCenterX()+posX-2, (int)shape.getCenterY()+posY-2);
+			c3 = new Circle(3,(int)shape.getCenterX()+posX-3, (int)shape.getCenterY()+posY-3);
 			
 			if(c1.intersects(e1)){
 				return true;
@@ -242,15 +242,15 @@ public boolean checkCloseBulletColission(Bullet b, Shape tile){
 			Rectangle2D e1 = (Rectangle2D) tile;
 			Circle c1,c2,c3;
 			
-			c1 = new Circle(2,(int)shape.getCenterX()-2,(int)shape.getCenterY()-2);
+			c1 = new Circle(3,(int)shape.getCenterX()-3,(int)shape.getCenterY()-3);
 			
 			int posX = (int) (3*Math.cos(dir));
 			int posY = (int) (3*Math.sin(dir));
-			c2 = new Circle(2,(int)shape.getCenterX()+posX-2, (int)shape.getCenterY()+posY-2);
+			c2 = new Circle(3,(int)shape.getCenterX()+posX-3, (int)shape.getCenterY()+posY-3);
 			
 			posX = (int) (3*Math.cos(dir+Math.PI));
 			posY = (int) (3*Math.sin(dir+Math.PI));
-			c3 = new Circle(2,(int)shape.getCenterX()+posX-2, (int)shape.getCenterY()+posY-2);
+			c3 = new Circle(3,(int)shape.getCenterX()+posX-3, (int)shape.getCenterY()+posY-3);
 			
 			if(c1.intersects(e1)){
 				return true;
