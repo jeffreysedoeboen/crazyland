18ac348ef765b367e7d189f64ca13deff3de0826
marijnie28 <marijnie28@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-06-15 07:53:05 +0000

    meuk

--- src/server/model/tile/Tile.java
+++ src/server/model/tile/Tile.java
@@ -45,21 +45,6 @@ public class Tile implements WorldObject{
 		this.image = image;
 	}
 	
-	public boolean[][] getPixelMap(){
-		boolean[][] pmap = new boolean[32][32];
-		for(int i = 0; i < 32; i++){
-			for(int i2 = 0; i2 < 32; i2++){
-				if(this.image.getRGB(i, i2) == 0){
-					pmap[i2][i] = true;
-				}else{
-					pmap[i2][i] = false;
-				}
-			}
-		}
-		
-		return pmap;
-	}
-	
 	public boolean isSolid() {
 		return solid;
 	}
