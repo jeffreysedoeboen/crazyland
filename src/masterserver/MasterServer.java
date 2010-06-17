567fd558ee7580c1ce5a02767d468c1e55bdd043
marco.schultewolter@googlemail.com <marco.schultewolter@googlemail.com@5680d515-0e07-e4fc-9cb2-9dcc4859aa7a>
2010-06-17 08:08:21 +0000

    masterserver connect bug fixed

--- src/masterserver/MasterServer.java
+++ src/masterserver/MasterServer.java
@@ -6,7 +6,6 @@ import masterserver.connection.*;
 
 public class MasterServer extends Thread{
 	private int guestCount = 0;
-	private ArrayList<Connection> connectionList = new ArrayList<Connection>();
 	
 	public static void main(String args[]){
 			
@@ -14,10 +13,6 @@ public class MasterServer extends Thread{
 		
 		new Connector(server).start();
 
-	}
-
-	public void addConnection(Connection c) {
-		this.connectionList.add(c);		
 	}
 	
 	public int getNewGuestNumber() {
